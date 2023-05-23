4837d2e817f590c794a85c505d468ccec4cbf0d7
RanggaCasper <hyfanutama@gmail.com>
2023-05-23 14:49:06 +0800

    add file kedalam repo

---
+++ kelas-2c/2215354079/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Belajar Git</title>
+</head>
+<body>
+	<h2 style="display: flex; justify-content: center;"><?php echo "Hello World"; ?></h2>
+</body>
+</html>
